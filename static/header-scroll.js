f6eb165732c4dd52c4deb558385fb971438201c2
Eric Scouten <eric@scouten.com>
2021-04-16 14:26:20 -0700

    Ooh, yeah, that transition is looking really sweet!

--- static/header-scroll.js
+++ static/header-scroll.js
@@ -1,8 +1,15 @@
 function resizeHeaderOnScroll() {
     const distanceY = window.pageYOffset || document.documentElement.scrollTop,
     shrinkOn = 40,
+    titleAppear = 415,
     headerEl = document.getElementById('header');
     
+    if (distanceY > titleAppear) {
+        headerEl.classList.add("add-title");
+    } else {
+        headerEl.classList.remove("add-title");
+    }
+
     if (distanceY > shrinkOn) {
         headerEl.classList.add("smaller");
     } else {
