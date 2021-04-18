47bcda80164458616ab3a379e78198eebd1e7400
Eric Scouten <eric@scouten.com>
2021-04-17 21:30:08 -0700

    Helps to have accurate markers!

--- content/parks/spencer-spit/markers.js
+++ content/parks/spencer-spit/markers.js
@@ -22,12 +22,12 @@ function addGpxMarker(map, id, lat, lon) {
 }
 
 function addGpxMarkers(map) {
-    addGpxMarker(map, "es-4126-050", 48.5353646, -122.859847);
-    addGpxMarker(map, "es-4126-051", 48.5353091, -122.8592665);
-    addGpxMarker(map, "es-4127-013", 48.5353556, -122.859216);
-    addGpxMarker(map, "es-4127-017", 48.5353556, -122.859216);
-    addGpxMarker(map, "es-4126-052", 48.5357174, -122.8578331);
-    addGpxMarker(map, "es-4127-021", 48.5357163, -122.8578298);
+    addGpxMarker(map, "es-4126-050", 48.535361, -122.857658);
+    addGpxMarker(map, "es-4126-051", 48.535306, -122.857078);
+    addGpxMarker(map, "es-4127-013", 48.535352, -122.857027);
+    addGpxMarker(map, "es-4127-017", 48.535497, -122.856477);
+    addGpxMarker(map, "es-4126-052", 48.535714, -122.855644);
+    addGpxMarker(map, "es-4127-021", 48.535713, -122.855641);
     addGpxMarker(map, "es-4127-025", 48.536609, -122.852502);
     addGpxMarker(map, "es-4127-026", 48.536684, -122.852275);
     addGpxMarker(map, "es-4127-028", 48.536609, -122.851686);
