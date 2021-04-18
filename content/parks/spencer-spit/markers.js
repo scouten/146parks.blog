1c87096004a8462cc0e1bae9f6a2bd11c3c6bf47
Eric Scouten <eric@scouten.com>
2021-04-17 21:24:39 -0700

    Rough cut of many image markers. Not so wonderful yet.

--- content/parks/spencer-spit/markers.js
+++ content/parks/spencer-spit/markers.js
@@ -22,6 +22,15 @@ function addGpxMarker(map, id, lat, lon) {
 }
 
 function addGpxMarkers(map) {
-    addGpxMarker(map, "es-4126-050", 48.5353646,-122.859847);
-    // and a few more ...
+    addGpxMarker(map, "es-4126-050", 48.5353646, -122.859847);
+    addGpxMarker(map, "es-4126-051", 48.5353091, -122.8592665);
+    addGpxMarker(map, "es-4127-013", 48.5353556, -122.859216);
+    addGpxMarker(map, "es-4127-017", 48.5353556, -122.859216);
+    addGpxMarker(map, "es-4126-052", 48.5357174, -122.8578331);
+    addGpxMarker(map, "es-4127-021", 48.5357163, -122.8578298);
+    addGpxMarker(map, "es-4127-025", 48.536609, -122.852502);
+    addGpxMarker(map, "es-4127-026", 48.536684, -122.852275);
+    addGpxMarker(map, "es-4127-028", 48.536609, -122.851686);
+    addGpxMarker(map, "es-4126-061", 48.536358, -122.852775);
+    addGpxMarker(map, "es-4127-042", 48.535371, -122.857749);
 }
