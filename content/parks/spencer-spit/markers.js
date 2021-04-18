2538f4462ed06faf07f90b67b6ee0cd4b9c6d8c3
Eric Scouten <eric@scouten.com>
2021-04-17 22:20:24 -0700

    Add clustering.

--- content/parks/spencer-spit/markers.js
+++ content/parks/spencer-spit/markers.js
@@ -1,13 +1,15 @@
 function addGpxMarkers(map) {
-    addGpxMarker(map, "es-4126-050", 48.535361, -122.857658);
-    addGpxMarker(map, "es-4126-051", 48.535306, -122.857078);
-    addGpxMarker(map, "es-4127-013", 48.535352, -122.857027);
-    addGpxMarker(map, "es-4127-017", 48.535497, -122.856477);
-    addGpxMarker(map, "es-4126-052", 48.535714, -122.855644);
-    addGpxMarker(map, "es-4127-021", 48.535713, -122.855641);
-    addGpxMarker(map, "es-4127-025", 48.536609, -122.852502);
-    addGpxMarker(map, "es-4127-026", 48.536684, -122.852275);
-    addGpxMarker(map, "es-4127-028", 48.536609, -122.851686);
-    addGpxMarker(map, "es-4126-061", 48.536358, -122.852775);
-    addGpxMarker(map, "es-4127-042", 48.535371, -122.857749);
+    return [
+        addGpxMarker(map, "es-4126-050", 48.535361, -122.857658),
+        addGpxMarker(map, "es-4126-051", 48.535306, -122.857078),
+        addGpxMarker(map, "es-4127-013", 48.535352, -122.857027),
+        addGpxMarker(map, "es-4127-017", 48.535497, -122.856477),
+        addGpxMarker(map, "es-4126-052", 48.535714, -122.855644),
+        addGpxMarker(map, "es-4127-021", 48.535713, -122.855641),
+        addGpxMarker(map, "es-4127-025", 48.536609, -122.852502),
+        addGpxMarker(map, "es-4127-026", 48.536684, -122.852275),
+        addGpxMarker(map, "es-4127-028", 48.536609, -122.851686),
+        addGpxMarker(map, "es-4126-061", 48.536358, -122.852775),
+        addGpxMarker(map, "es-4127-042", 48.535371, -122.857749),
+    ]
 }
