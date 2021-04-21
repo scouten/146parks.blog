8045d7860d37777e4c40d89fcf8ee784ad2e6dfe
Eric Scouten <eric@scouten.com>
2021-04-20 19:59:08 -0500

    Photo location markers.

---
+++ content/parks/lime-kiln-point/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4126-100", 48.515675, -123.152061),
+        addGpxMarker(map, "es-4127-044", 48.515707, -123.151912),
+        addGpxMarker(map, "es-4127-046", 48.515707, -123.151912),
+        addGpxMarker(map, "es-4126-105", 48.515808, -123.152222),
+        addGpxMarker(map, "es-4126-113", 48.518672, -123.151322),
+        addGpxMarker(map, "es-4126-129", 48.518725, -123.151397),
+        addGpxMarker(map, "es-4126-133", 48.518694, -123.151406),
+        addGpxMarker(map, "es-4126-141", 48.518647, -123.151528),
+        addGpxMarker(map, "es-4126-144", 48.518725, -123.151536),
+        addGpxMarker(map, "es-4130-009", 48.515200, -123.151700),
+        addGpxMarker(map, "es-4130-015", 48.515336, -123.151772),
+        addGpxMarker(map, "es-4128-034", 48.515347, -123.151719),
+    ]
+}
