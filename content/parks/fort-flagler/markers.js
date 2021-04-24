9a63cb0fc60bd8715528fe2b1357af9b9d2c3f73
Eric Scouten <eric@scouten.com>
2021-04-23 21:26:09 -0600

    Fort Flagler SP.
    Closes #30.

---
+++ content/parks/fort-flagler/markers.js
@@ -0,0 +1,19 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4131-047", 48.092997, -122.720872),
+        addGpxMarker(map, "es-4132-001", 48.097773, -122.715498),
+        addGpxMarker(map, "es-4132-002", 48.097813, -122.715494),
+        addGpxMarker(map, "es-4131-051", 48.097953, -122.715553),
+        addGpxMarker(map, "es-4131-053", 48.097942, -122.715461),
+        addGpxMarker(map, "es-4131-055", 48.097869, -122.715317),
+        addGpxMarker(map, "es-4131-058", 48.096522, -122.717919),
+        addGpxMarker(map, "es-4131-061", 48.095936, -122.721481),
+        addGpxMarker(map, "es-4131-064", 48.095847, -122.721825),
+        addGpxMarker(map, "es-4131-092", 48.095256, -122.723228),
+        addGpxMarker(map, "es-4131-093", 48.094078, -122.724044),
+        addGpxMarker(map, "es-4132-019", 48.092901, -122.706718),
+        addGpxMarker(map, "es-4132-028", 48.092832, -122.706201),
+        addGpxMarker(map, "es-4131-100", 48.092953, -122.705864),
+        addGpxMarker(map, "es-4131-103", 48.097994, -122.694428),
+    ]
+}
