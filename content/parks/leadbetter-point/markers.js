429d5f828411661b320d043ad9264d41308547df
Eric Scouten <eric@scouten.com>
2021-04-24 20:27:43 -0600

    Leadbetter Point SP.
    Closes #63.

---
+++ content/parks/leadbetter-point/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4146-075", 46.606708, -124.041747),
+        addGpxMarker(map, "es-4147-072", 46.608788, -124.043917),
+        addGpxMarker(map, "es-4147-074", 46.609189, -124.044306),
+        addGpxMarker(map, "es-4147-077", 46.609427, -124.044458),
+        addGpxMarker(map, "es-4146-081", 46.604253, -124.053344),
+        addGpxMarker(map, "es-4146-097", 46.605842, -124.064025),
+        addGpxMarker(map, "es-4146-099", 46.605806, -124.066475),
+        addGpxMarker(map, "es-4146-115", 46.606128, -124.069375),
+        addGpxMarker(map, "es-4146-116", 46.606136, -124.069039),
+        addGpxMarker(map, "es-4146-118", 46.606056, -124.068833),
+        addGpxMarker(map, "es-4146-122", 46.605881, -124.068497),
+    ]
+}
