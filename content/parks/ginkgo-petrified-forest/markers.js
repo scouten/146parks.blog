92f0f84346d064f1f569653eca15eb5031837c3d
Eric Scouten <eric@scouten.com>
2021-04-29 16:04:47 -0700

    Ginkgo Petrified Forest SP.
    Closes #148.

---
+++ content/parks/ginkgo-petrified-forest/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4176-009", 46.947758, -120.035950),
+        addGpxMarker(map, "es-4176-012p", 46.954819, -119.987931),
+        addGpxMarker(map, "es-4176-032", 46.954833, -119.988014),
+        addGpxMarker(map, "es-4176-039", 46.954769, -119.988083),
+        addGpxMarker(map, "es-4176-040", 46.948044, -120.035531),
+        addGpxMarker(map, "es-4176-042", 46.948533, -120.034500),
+        addGpxMarker(map, "es-4176-043", 46.948964, -120.032753),
+        addGpxMarker(map, "es-4176-045", 46.949425, -120.032814),
+        addGpxMarker(map, "es-4176-132", 46.950919, -120.040833),
+        addGpxMarker(map, "es-4176-143", 46.949631, -120.040261),
+        addGpxMarker(map, "es-4176-149", 46.948508, -120.035217),
+    ]
+}
