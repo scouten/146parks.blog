0f2b1600092cd33355b9ecb20c798838527c576f
Eric Scouten <eric@scouten.com>
2021-04-29 15:35:47 -0700

    Wenatchee Confluence SP.
    Closes #20.

---
+++ content/parks/wenatchee-confluence/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4173-056p", 47.461114, -120.322669),
+        addGpxMarker(map, "es-4173-067", 47.461431, -120.322700),
+        addGpxMarker(map, "es-4173-068", 47.461219, -120.323350),
+        addGpxMarker(map, "es-4173-074", 47.458808, -120.324342),
+        addGpxMarker(map, "es-4173-076", 47.458856, -120.324394),
+        addGpxMarker(map, "es-4173-079", 47.458764, -120.325036),
+        addGpxMarker(map, "es-4173-080", 47.458775, -120.326553),
+    ]
+}
