ffa057002f6edd4f441543b493400f8d25cb6dcf
Eric Scouten <eric@scouten.com>
2021-04-29 09:28:05 -0700

    Kopachuck SP.
    Closes #88.

---
+++ content/parks/kopachuck/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4166-022", 47.308922, -122.686219),
+        addGpxMarker(map, "es-4166-024", 47.310569, -122.686669),
+        addGpxMarker(map, "es-4166-032", 47.310617, -122.686547),
+        addGpxMarker(map, "es-4166-033", 47.310650, -122.686547),
+        addGpxMarker(map, "es-4166-039", 47.310311, -122.687181),
+        addGpxMarker(map, "es-4166-042", 47.310153, -122.687469),
+        addGpxMarker(map, "es-4166-044", 47.309911, -122.687303),
+        addGpxMarker(map, "es-4166-047", 47.310617, -122.685653),
+        addGpxMarker(map, "es-4166-049", 47.310650, -122.685508),
+        addGpxMarker(map, "es-4166-057", 47.308869, -122.684014),
+    ]
+}
