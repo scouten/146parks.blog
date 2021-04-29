92d117ac922ae424142efd4cea329a361cf7c614
Eric Scouten <eric@scouten.com>
2021-04-29 14:14:40 -0700

    Bogachiel SP.
    Closes #56.

---
+++ content/parks/bogachiel/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4167-027", 47.893386, -124.363189),
+        addGpxMarker(map, "es-4167-028", 47.893478, -124.363319),
+        addGpxMarker(map, "es-4167-030", 47.893364, -124.363381),
+        addGpxMarker(map, "es-4167-031", 47.893353, -124.363564),
+        addGpxMarker(map, "es-4167-032", 47.893211, -124.363708),
+        addGpxMarker(map, "es-4167-045", 47.894058, -124.361900),
+        addGpxMarker(map, "es-4167-049", 47.894239, -124.358658),
+        addGpxMarker(map, "es-4167-054", 47.894150, -124.358047),
+        addGpxMarker(map, "es-4167-055", 47.894225, -124.357947),
+        addGpxMarker(map, "es-4167-058", 47.894322, -124.361236),
+        addGpxMarker(map, "es-4167-060", 47.893944, -124.362672),
+    ]
+}
