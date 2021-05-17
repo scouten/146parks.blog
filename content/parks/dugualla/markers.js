4e9ff124421ffe65ae5e215694c9198e6e6f9e44
Eric Scouten <eric@scouten.com>
2021-05-17 16:56:08 -0700

    Add track log.

---
+++ content/parks/dugualla/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4201-003", 48.340367, -122.571594),
+        addGpxMarker(map, "es-4201-011p", 48.342419, -122.561492),
+        addGpxMarker(map, "es-4201-030", 48.342628, -122.561400),
+        addGpxMarker(map, "es-4201-034", 48.341719, -122.559028),
+        addGpxMarker(map, "es-4201-042", 48.343331, -122.555633),
+        addGpxMarker(map, "es-4201-048p", 48.343472, -122.555472),
+        addGpxMarker(map, "es-4201-070", 48.343472, -122.555336),
+        addGpxMarker(map, "es-4201-073", 48.343419, -122.555450),
+        addGpxMarker(map, "es-4201-074", 48.343342, -122.555519),
+        addGpxMarker(map, "es-4201-082", 48.341217, -122.557564),
+        addGpxMarker(map, "es-4201-088", 48.340133, -122.555825),
+        addGpxMarker(map, "es-4201-101", 48.335153, -122.564369),
+    ]
+}
