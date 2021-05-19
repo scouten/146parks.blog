34bb3d21ca646e712d489dd6c76d1ff515095e7b
Eric Scouten <eric@scouten.com>
2021-05-18 22:50:14 -0700

    Shine Tidelands SP.
    Closes #23.

---
+++ content/parks/shine-tidelands/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4202-037", 47.867589, -122.635703),
+        addGpxMarker(map, "es-4202-045", 47.868778, -122.634772),
+        addGpxMarker(map, "es-4202-048p", 47.869594, -122.634597),
+        addGpxMarker(map, "es-4202-055", 47.870078, -122.634108),
+        addGpxMarker(map, "es-4202-066", 47.871197, -122.633614),
+        addGpxMarker(map, "es-4202-069p", 47.871353, -122.633575),
+        addGpxMarker(map, "es-4202-077", 47.871822, -122.633408),
+        addGpxMarker(map, "es-4202-079", 47.872344, -122.633164),
+        addGpxMarker(map, "es-4202-086", 47.871944, -122.633392),
+        addGpxMarker(map, "es-4202-090", 47.871794, -122.633444),
+        addGpxMarker(map, "es-4202-095", 47.871464, -122.633592),
+        addGpxMarker(map, "es-4202-100", 47.871403, -122.633536),
+        addGpxMarker(map, "es-4202-112", 47.869050, -122.634492),
+        addGpxMarker(map, "es-4202-124", 47.881961, -122.630614),
+    ]
+}
