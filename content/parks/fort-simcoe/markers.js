e6d0ce9ce9950572c33cdc77366f5c557e5abeb1
Eric Scouten <eric@scouten.com>
2021-06-10 20:28:40 -0700

    Fort Simcoe SP.
    Closes #103.

---
+++ content/parks/fort-simcoe/markers.js
@@ -0,0 +1,20 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4213-196p", 46.342817, -120.838736),
+        addGpxMarker(map, "es-4213-218", 46.342514, -120.839644),
+        addGpxMarker(map, "es-4213-220", 46.342378, -120.839967),
+        addGpxMarker(map, "es-4213-235", 46.342792, -120.840683),
+        addGpxMarker(map, "es-4213-258", 46.341908, -120.839653),
+        addGpxMarker(map, "es-4213-261p", 46.341542, -120.839339),
+        addGpxMarker(map, "es-4213-280", 46.340642, -120.839675),
+        addGpxMarker(map, "es-4213-283p", 46.340650, -120.839578),
+        addGpxMarker(map, "es-4213-303", 46.340467, -120.839928),
+        addGpxMarker(map, "es-4213-311", 46.340619, -120.839608),
+        addGpxMarker(map, "es-4213-324", 46.342158, -120.839797),
+        addGpxMarker(map, "es-4213-326", 46.342219, -120.839814),
+        addGpxMarker(map, "es-4213-327", 46.342256, -120.839850),
+        addGpxMarker(map, "es-4213-328p", 46.342261, -120.839850),
+        addGpxMarker(map, "es-4213-330", 46.342125, -120.838722),
+        addGpxMarker(map, "es-4213-355", 46.341853, -120.834944),
+    ]
+}
