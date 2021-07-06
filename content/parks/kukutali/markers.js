43b18bdab9e2e3a422848a88443fdf5b2402c3f4
Eric Scouten <eric@scouten.com>
2021-07-06 15:34:33 -0500

    WIP on Kukutali Preserve.

---
+++ content/parks/kukutali/markers.js
@@ -0,0 +1,19 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4221-005", 48.420892, -122.553969),
+        addGpxMarker(map, "es-4221-007", 48.420933, -122.556367),
+        addGpxMarker(map, "es-4221-009", 48.420914, -122.556419),
+        addGpxMarker(map, "es-4221-010", 48.420897, -122.558647),
+        addGpxMarker(map, "es-4221-028", 48.420053, -122.561089),
+        addGpxMarker(map, "es-4221-029", 48.419875, -122.562919),
+        addGpxMarker(map, "es-4221-035", 48.417839, -122.570717),
+        addGpxMarker(map, "es-4221-055", 48.418025, -122.571433),
+        addGpxMarker(map, "es-4221-056", 48.417703, -122.571578),
+        addGpxMarker(map, "es-4221-057", 48.417694, -122.571669),
+        addGpxMarker(map, "es-4221-059", 48.417614, -122.572228),
+        addGpxMarker(map, "es-4221-068", 48.419719, -122.563361),
+        addGpxMarker(map, "es-4221-069", 48.420247, -122.565492),
+        addGpxMarker(map, "es-4221-070", 48.419931, -122.568275),
+        addGpxMarker(map, "es-4221-071", 48.419594, -122.569275),
+    ]
+}
