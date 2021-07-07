0deecc470fcd3895f82cb28b4eed6eeccc2d6c9f
Eric Scouten <eric@scouten.com>
2021-07-07 14:57:52 -0700

    Hope Island SP (of Skagit County).
    Closes #141.

---
+++ content/parks/hope-island-skagit/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4221-200", 48.407306, -122.568900),
+        addGpxMarker(map, "es-4221-208", 48.397675, -122.561158),
+        addGpxMarker(map, "es-4221-210", 48.397750, -122.561281),
+        addGpxMarker(map, "es-4221-211", 48.397750, -122.561281),
+        addGpxMarker(map, "es-4221-214", 48.397714, -122.562203),
+        addGpxMarker(map, "es-4221-240", 48.395339, -122.577217),
+        addGpxMarker(map, "es-4221-245", 48.398992, -122.570808),
+        addGpxMarker(map, "es-4221-246p", 48.396044, -122.570594),
+        addGpxMarker(map, "es-4221-268p", 48.396019, -122.570564),
+        addGpxMarker(map, "es-4221-274", 48.397317, -122.571014),
+        addGpxMarker(map, "es-4221-275", 48.399206, -122.571036),
+        addGpxMarker(map, "es-4221-276", 48.400250, -122.571481),
+        addGpxMarker(map, "es-4221-278", 48.401303, -122.571731),
+    ]
+}
