64d29951c5e524f515bd51043b4f9cf6a76e3dbd
Eric Scouten <eric@scouten.com>
2021-07-27 08:15:41 -0700

    Sequim Bay SP.
    Closes #27.

---
+++ content/parks/sequim-bay/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4224-046", 48.043289, -123.028275),
+        addGpxMarker(map, "es-4224-048", 48.043044, -123.027961),
+        addGpxMarker(map, "es-4224-052", 48.040794, -123.028367),
+        addGpxMarker(map, "es-4224-053", 48.040433, -123.027681),
+        addGpxMarker(map, "es-4224-054", 48.040025, -123.027358),
+        addGpxMarker(map, "es-4224-069", 48.040439, -123.026000),
+        addGpxMarker(map, "es-4224-072", 48.040561, -123.025650),
+        addGpxMarker(map, "es-4224-076", 48.040711, -123.026267),
+        addGpxMarker(map, "es-4224-080", 48.041131, -123.026750),
+        addGpxMarker(map, "es-4224-085", 48.039981, -123.028131),
+        addGpxMarker(map, "es-4224-090", 48.040672, -123.028672),
+    ]
+}
