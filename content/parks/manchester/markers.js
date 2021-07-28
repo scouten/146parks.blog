159d27fe04652c19cf52a194ec5a431b77d2de72
Eric Scouten <eric@scouten.com>
2021-07-27 20:11:47 -0700

    Manchester SP.
    Closes #117.

---
+++ content/parks/manchester/markers.js
@@ -0,0 +1,19 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4226-010", 47.576978, -122.545144),
+        addGpxMarker(map, "es-4226-020", 47.578103, -122.546861),
+        addGpxMarker(map, "es-4226-021", 47.578094, -122.547119),
+        addGpxMarker(map, "es-4226-024", 47.578214, -122.548156),
+        addGpxMarker(map, "es-4226-025", 47.577839, -122.549797),
+        addGpxMarker(map, "es-4226-031", 47.577717, -122.549889),
+        addGpxMarker(map, "es-4226-033", 47.577633, -122.550086),
+        addGpxMarker(map, "es-4226-041", 47.577331, -122.550172),
+        addGpxMarker(map, "es-4226-043", 47.577050, -122.550583),
+        addGpxMarker(map, "es-4226-047", 47.577686, -122.549447),
+        addGpxMarker(map, "es-4226-050", 47.578286, -122.549836),
+        addGpxMarker(map, "es-4226-086", 47.578269, -122.549408),
+        addGpxMarker(map, "es-4226-090", 47.578275, -122.549233),
+        addGpxMarker(map, "es-4226-091", 47.578297, -122.549225),
+        addGpxMarker(map, "es-4228-015", 47.577766, -122.549890),
+    ]
+}
