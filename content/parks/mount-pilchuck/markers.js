a423aab605d2c2f5796718e9782d863ee36f88f9
Eric Scouten <eric@scouten.com>
2021-08-09 20:33:54 -0700

    Mount Pilchuck SP.
    Closes #53.

---
+++ content/parks/mount-pilchuck/markers.js
@@ -0,0 +1,23 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4236-005", 48.066044, -121.814522),
+        addGpxMarker(map, "es-4236-007", 48.063222, -121.816428),
+        addGpxMarker(map, "es-4236-009", 48.061325, -121.815711),
+        addGpxMarker(map, "es-4236-016", 48.059883, -121.813339),
+        addGpxMarker(map, "es-4236-019", 48.061981, -121.811578),
+        addGpxMarker(map, "es-4236-047", 48.062406, -121.807944),
+        addGpxMarker(map, "es-4236-062", 48.061114, -121.804161),
+        addGpxMarker(map, "es-4236-065", 48.060567, -121.803353),
+        addGpxMarker(map, "es-4236-071", 48.060581, -121.803367),
+        addGpxMarker(map, "es-4236-096", 48.057628, -121.800194),
+        addGpxMarker(map, "es-4236-102", 48.057400, -121.797028),
+        addGpxMarker(map, "jp-20210725_151828", 48.057778, -121.797778),
+        addGpxMarker(map, "es-4236-105", 48.057772, -121.797517),
+        addGpxMarker(map, "es-4236-108", 48.057797, -121.797578),
+        addGpxMarker(map, "jp-20210725_152537", 48.057985, -121.797794),
+        addGpxMarker(map, "jp-20210725_153647", 48.057928, -121.797823),
+        addGpxMarker(map, "es-4236-109", 48.057664, -121.797119),
+        addGpxMarker(map, "es-4236-111", 48.057675, -121.797408),
+        addGpxMarker(map, "es-4236-117", 48.061225, -121.804328),
+    ]
+}
