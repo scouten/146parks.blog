b927be896e78d316f6b1734e7a311cc169ea1e4d
Eric Scouten <eric@scouten.com>
2021-08-12 22:58:25 -0700

    McMicken Island SP.
    Closes #109.

---
+++ content/parks/mcmicken-island/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4237-024", 47.225094, -122.809608),
+        addGpxMarker(map, "es-4237-030p", 47.224864, -122.813758),
+        addGpxMarker(map, "es-4237-036", 47.226403, -122.819192),
+        addGpxMarker(map, "es-4237-046", 47.249958, -122.859092),
+        addGpxMarker(map, "es-4237-058", 47.247436, -122.864800),
+        addGpxMarker(map, "es-4237-059p", 47.247464, -122.864158),
+        addGpxMarker(map, "es-4237-083", 47.247569, -122.864075),
+        addGpxMarker(map, "es-4237-087", 47.247761, -122.863092),
+        addGpxMarker(map, "es-4237-088", 47.248328, -122.862731),
+        addGpxMarker(map, "es-4237-089", 47.248542, -122.862403),
+        addGpxMarker(map, "es-4237-091", 47.248967, -122.861589),
+        addGpxMarker(map, "es-4237-117", 47.246933, -122.865556),
+        addGpxMarker(map, "es-4237-123", 47.247508, -122.871231),
+        addGpxMarker(map, "es-4237-127", 47.247661, -122.871392),
+    ]
+}
