b9ee389982f21ce01703992edf7c1772ad654caa
Eric Scouten <eric@scouten.com>
2021-09-03 15:27:32 -0700

    Twin Harbors SP.
    Closes #65.

---
+++ content/parks/twin-harbors/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4244-020", 46.858289, -124.113136),
+        addGpxMarker(map, "es-4244-021", 46.857444, -124.113342),
+        addGpxMarker(map, "es-4244-024", 46.857425, -124.113486),
+        addGpxMarker(map, "es-4244-028", 46.857333, -124.113769),
+        addGpxMarker(map, "es-4244-034", 46.856694, -124.113603),
+        addGpxMarker(map, "es-4244-035", 46.856639, -124.113883),
+        addGpxMarker(map, "es-4244-037", 46.856617, -124.114006),
+        addGpxMarker(map, "es-4244-039", 46.856108, -124.115922),
+        addGpxMarker(map, "es-4244-042", 46.845731, -124.112289),
+        addGpxMarker(map, "es-4244-044", 46.849411, -124.113533),
+        addGpxMarker(map, "es-4244-045", 46.849444, -124.113533),
+        addGpxMarker(map, "es-4244-046", 46.858653, -124.115028),
+        addGpxMarker(map, "es-4244-047", 46.858658, -124.115019),
+        addGpxMarker(map, "es-4244-049", 46.858678, -124.114861),
+    ]
+}
