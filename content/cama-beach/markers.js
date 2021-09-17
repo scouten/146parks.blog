6db7a25e229c745e9197f305d91b728bf0f8cb61
Eric Scouten <eric@scouten.com>
2021-09-16 19:12:10 -0700

    Cama Beach SP.
    Closes #31.

---
+++ content/cama-beach/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4249-054", 48.143742, -122.514197),
+        addGpxMarker(map, "es-4249-056", 48.143300, -122.514442),
+        addGpxMarker(map, "es-4249-058", 48.142983, -122.514625),
+        addGpxMarker(map, "es-4249-074", 48.142964, -122.514497),
+        addGpxMarker(map, "es-4249-079", 48.142625, -122.515144),
+        addGpxMarker(map, "es-4249-084", 48.142611, -122.515219),
+        addGpxMarker(map, "es-4249-088", 48.142231, -122.515075),
+        addGpxMarker(map, "es-4249-092", 48.141894, -122.515319),
+        addGpxMarker(map, "es-4249-103", 48.140972, -122.514542),
+        addGpxMarker(map, "es-4249-106", 48.140644, -122.514397),
+        addGpxMarker(map, "es-4249-109", 48.140567, -122.514336),
+        addGpxMarker(map, "es-4249-112", 48.140053, -122.513864),
+        addGpxMarker(map, "es-4249-116", 48.139783, -122.513619),
+        addGpxMarker(map, "es-4249-134", 48.142028, -122.512603),
+    ]
+}
