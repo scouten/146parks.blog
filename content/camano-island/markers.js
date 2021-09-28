6bec1f780e2b9ee8d79972d966e418783075fcd7
Eric Scouten <eric@scouten.com>
2021-09-28 17:58:56 -0500

    Camano Island SP. Closes #32.

---
+++ content/camano-island/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4249-136", 48.124061, -122.494889),
+        addGpxMarker(map, "es-4249-137", 48.123967, -122.494933),
+        addGpxMarker(map, "es-4249-140", 48.123644, -122.494919),
+        addGpxMarker(map, "es-4249-143", 48.123292, -122.494758),
+        addGpxMarker(map, "es-4249-149", 48.122728, -122.494339),
+        addGpxMarker(map, "es-4249-162", 48.121911, -122.493081),
+        addGpxMarker(map, "es-4249-163", 48.121967, -122.493181),
+        addGpxMarker(map, "es-4249-174", 48.121028, -122.489089),
+        addGpxMarker(map, "es-4249-179", 48.121044, -122.489931),
+        addGpxMarker(map, "es-4249-181", 48.122264, -122.493789),
+        addGpxMarker(map, "es-4249-189", 48.121503, -122.487031),
+    ]
+}
