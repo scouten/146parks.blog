4ca8e097ae75350a408cc0fbe9082815c569bbf5
Eric Scouten <eric@scouten.com>
2021-10-02 20:02:29 -0700

    Saint Edward SP.
    Closes #90.

---
+++ content/saint-edward/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4256-014", 47.733514, -122.257264),
+        addGpxMarker(map, "es-4256-041", 47.735622, -122.256942),
+        addGpxMarker(map, "es-4256-042", 47.735522, -122.257417),
+        addGpxMarker(map, "es-4256-048", 47.735242, -122.260558),
+        addGpxMarker(map, "es-4256-050", 47.735211, -122.264358),
+        addGpxMarker(map, "es-4256-055", 47.734647, -122.264847),
+        addGpxMarker(map, "es-4256-058", 47.733525, -122.264556),
+        addGpxMarker(map, "es-4256-062", 47.732697, -122.264358),
+        addGpxMarker(map, "es-4256-066", 47.732689, -122.264411),
+        addGpxMarker(map, "es-4256-072", 47.732700, -122.264397),
+        addGpxMarker(map, "es-4256-080", 47.731019, -122.263306),
+        addGpxMarker(map, "es-4256-112", 47.729156, -122.263047),
+        addGpxMarker(map, "es-4256-114", 47.731975, -122.257797),
+    ]
+}
