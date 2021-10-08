d9ac40e2b2de988c4161760a7699e4785606784a
Eric Scouten <eric@scouten.com>
2021-10-08 14:57:00 -0700

    Belfair SP.
    Closes #113.

---
+++ content/belfair/markers.js
@@ -0,0 +1,11 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4260-199", 47.430742, -122.874328),
+        addGpxMarker(map, "es-4260-212", 47.429681, -122.874519),
+        addGpxMarker(map, "es-4260-221", 47.428792, -122.875253),
+        addGpxMarker(map, "es-4260-228", 47.429419, -122.875289),
+        addGpxMarker(map, "es-4260-234", 47.430069, -122.879081),
+        addGpxMarker(map, "es-4260-238", 47.430039, -122.881019),
+        addGpxMarker(map, "es-4260-240", 47.429764, -122.881425),
+    ]
+}
