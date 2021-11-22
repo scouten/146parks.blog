0b67d02550474bae6bfe0fc878f0681bb90736c2
Eric Scouten <eric@scouten.com>
2021-11-21 22:06:52 -0800

    Scenic Beach SP.

---
+++ content/scenic-beach/markers.js
@@ -0,0 +1,20 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4269-048", 47.649125, -122.847319),
+        addGpxMarker(map, "es-4269-054", 47.649497, -122.847319),
+        addGpxMarker(map, "es-4269-055a", 47.649639, -122.847458),
+        addGpxMarker(map, "es-4269-056", 47.649544, -122.847381),
+        addGpxMarker(map, "es-4269-058", 47.649567, -122.847481),
+        addGpxMarker(map, "es-4269-066", 47.649414, -122.846261),
+        addGpxMarker(map, "es-4269-070", 47.648811, -122.844636),
+        addGpxMarker(map, "es-4269-082", 47.648228, -122.842942),
+        addGpxMarker(map, "es-4269-084", 47.647892, -122.842903),
+        addGpxMarker(map, "es-4269-086", 47.646778, -122.842819),
+        addGpxMarker(map, "es-4269-088", 47.646564, -122.842636),
+        addGpxMarker(map, "es-4269-092", 47.645847, -122.842300),
+        addGpxMarker(map, "es-4269-103", 47.644244, -122.842864),
+        addGpxMarker(map, "es-4269-119p", 47.649964, -122.846869),
+        addGpxMarker(map, "es-4269-148", 47.649917, -122.846886),
+        addGpxMarker(map, "es-4269-156", 47.649925, -122.847017),
+    ]
+}
