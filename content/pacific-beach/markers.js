e9b165b6bef6e07529c61d05425225768958ef12
Eric Scouten <eric@scouten.com>
2022-01-16 17:19:22 -0800

    Pacific Beach SP. Closes #69.

---
+++ content/pacific-beach/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4303-080", 47.206578, -124.203767),
+        addGpxMarker(map, "es-4303-082", 47.206550, -124.203956),
+        addGpxMarker(map, "es-4303-100", 47.206611, -124.204292),
+        addGpxMarker(map, "es-4303-103", 47.206964, -124.205117),
+        addGpxMarker(map, "es-4303-105", 47.206500, -124.205292),
+        addGpxMarker(map, "es-4303-108", 47.206117, -124.204644),
+        addGpxMarker(map, "es-4303-110", 47.206153, -124.204467),
+        addGpxMarker(map, "es-4303-112", 47.205467, -124.204308),
+        addGpxMarker(map, "es-4303-120", 47.204300, -124.202300),
+        addGpxMarker(map, "es-4303-123", 47.204300, -124.202369),
+        addGpxMarker(map, "es-4303-140", 47.204528, -124.202981),
+        addGpxMarker(map, "es-4303-148", 47.205192, -124.203653),
+        addGpxMarker(map, "es-4303-150", 47.205994, -124.203622),
+    ]
+}
