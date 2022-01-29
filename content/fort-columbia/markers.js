98a05f03fcc52cd1bcd8fc4236d983be49c30b51
Eric Scouten <eric@scouten.com>
2022-01-29 13:01:42 -0800

    Fort Columbia SP.
    Closes #59.

---
+++ content/fort-columbia/markers.js
@@ -0,0 +1,23 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4308-150", 46.253283, -123.920533),
+        addGpxMarker(map, "es-4308-162", 46.253044, -123.919861),
+        addGpxMarker(map, "es-4308-165", 46.252439, -123.918975),
+        addGpxMarker(map, "es-4308-169", 46.252419, -123.918397),
+        addGpxMarker(map, "es-4308-173", 46.252525, -123.918481),
+        addGpxMarker(map, "es-4308-184", 46.252372, -123.919511),
+        addGpxMarker(map, "es-4308-195", 46.252122, -123.921181),
+        addGpxMarker(map, "es-4308-198", 46.251861, -123.920511),
+        addGpxMarker(map, "es-4308-202", 46.251581, -123.920250),
+        addGpxMarker(map, "es-4308-211", 46.251228, -123.922256),
+        addGpxMarker(map, "es-4308-216", 46.252483, -123.923186),
+        addGpxMarker(map, "es-4308-230", 46.251928, -123.923744),
+        addGpxMarker(map, "es-4308-239", 46.251881, -123.923753),
+        addGpxMarker(map, "es-4308-242", 46.251900, -123.923900),
+        addGpxMarker(map, "es-4308-252", 46.253025, -123.920836),
+        addGpxMarker(map, "es-4308-253", 46.252994, -123.920800),
+        addGpxMarker(map, "es-4308-258", 46.252578, -123.920281),
+        addGpxMarker(map, "es-4308-261", 46.252578, -123.920281),
+        addGpxMarker(map, "es-4308-262", 46.253014, -123.920753),
+    ]
+}
