448264d1e54178b9c55183fe99464e25bc3f6292
Eric Scouten <eric@scouten.com>
2022-01-30 19:34:46 -0800

    Loomis Lake SP.

---
+++ content/loomis-lake/markers.js
@@ -0,0 +1,14 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4308-278", 46.433489, -124.055947),
+        addGpxMarker(map, "es-4308-280", 46.433636, -124.057236),
+        addGpxMarker(map, "es-4308-282", 46.433669, -124.057878),
+        addGpxMarker(map, "es-4308-288", 46.433792, -124.058686),
+        addGpxMarker(map, "es-4308-291", 46.433875, -124.059708),
+        addGpxMarker(map, "es-4308-296", 46.433711, -124.060036),
+        addGpxMarker(map, "es-4308-297", 46.433594, -124.060119),
+        addGpxMarker(map, "es-4308-302", 46.433528, -124.060981),
+        addGpxMarker(map, "es-4308-306", 46.433597, -124.061050),
+        addGpxMarker(map, "es-4308-313", 46.433844, -124.060944),
+    ]
+}
