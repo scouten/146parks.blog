749e96f402bc637646ad48528f077e37ee8ce976
Eric Scouten <eric@scouten.com>
2022-02-09 17:48:51 -0600

    Doug's Beach SP. Closes #4.

---
+++ content/dougs-beach/markers.js
@@ -0,0 +1,16 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4309-029", 45.677897, -121.235053),
+        addGpxMarker(map, "es-4309-035", 45.677189, -121.235322),
+        addGpxMarker(map, "es-4309-054", 45.676956, -121.235406),
+        addGpxMarker(map, "es-4309-055", 45.676906, -121.235314),
+        addGpxMarker(map, "es-4309-058", 45.676533, -121.235161),
+        addGpxMarker(map, "es-4309-059", 45.676375, -121.234992),
+        addGpxMarker(map, "es-4309-062", 45.676308, -121.234711),
+        addGpxMarker(map, "es-4309-069", 45.676517, -121.233361),
+        addGpxMarker(map, "es-4309-070", 45.676250, -121.233331),
+        addGpxMarker(map, "es-4309-077", 45.676628, -121.231439),
+        addGpxMarker(map, "es-4309-079", 45.676483, -121.234917),
+        addGpxMarker(map, "es-4309-084", 45.678000, -121.235000),
+    ]
+}
