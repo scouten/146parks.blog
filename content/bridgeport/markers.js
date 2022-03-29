916c5e55b8491e999064ed548b397b4cb817d20c
Eric Scouten <eric@scouten.com>
2022-03-28 22:01:13 -0700

    Bridgeport SP. Closes #11.

---
+++ content/bridgeport/markers.js
@@ -0,0 +1,10 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4321-140", 48.015975, -119.604597),
+        addGpxMarker(map, "es-4321-141", 48.015975, -119.604583),
+        addGpxMarker(map, "es-4321-143", 48.016036, -119.605033),
+        addGpxMarker(map, "es-4321-144", 48.015328, -119.606178),
+        addGpxMarker(map, "es-4321-148", 48.014819, -119.606828),
+        addGpxMarker(map, "es-4321-154", 48.014556, -119.607561),
+    ]
+}
