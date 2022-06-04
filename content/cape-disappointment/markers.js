82976827b268c4afab54fa47aee5eb7072ed1761
Eric Scouten <eric@scouten.com>
2022-06-03 22:15:59 -0600

    Cape Disappointment SP. Closes #61.

---
+++ content/cape-disappointment/markers.js
@@ -0,0 +1,25 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4356-037", 46.278586, -124.052261),
+        addGpxMarker(map, "es-4356-039", 46.278472, -124.051811),
+        addGpxMarker(map, "es-4356-057", 46.277767, -124.053703),
+        addGpxMarker(map, "es-4356-066", 46.278356, -124.051797),
+        addGpxMarker(map, "es-4356-067", 46.278081, -124.050469),
+        addGpxMarker(map, "es-4356-073", 46.277503, -124.049147),
+        addGpxMarker(map, "es-4356-080", 46.276639, -124.050117),
+        addGpxMarker(map, "es-4356-093", 46.275911, -124.052339),
+        addGpxMarker(map, "es-4356-095", 46.275822, -124.052222),
+        addGpxMarker(map, "es-4356-130", 46.298147, -124.073547),
+        addGpxMarker(map, "es-4356-133", 46.298303, -124.073669),
+        addGpxMarker(map, "es-4356-138", 46.298344, -124.074189),
+        addGpxMarker(map, "es-4356-143", 46.297878, -124.074883),
+        addGpxMarker(map, "es-4356-145", 46.297944, -124.075714),
+        addGpxMarker(map, "es-4356-149", 46.298081, -124.077042),
+        addGpxMarker(map, "es-4356-150", 46.298100, -124.077100),
+        addGpxMarker(map, "es-4356-156", 46.298661, -124.077767),
+        addGpxMarker(map, "es-4356-166", 46.299000, -124.077928),
+        addGpxMarker(map, "es-4356-168", 46.298736, -124.076906),
+        addGpxMarker(map, "es-4356-171", 46.298725, -124.076919),
+        addGpxMarker(map, "es-4356-181", 46.298447, -124.074469),
+    ]
+}
