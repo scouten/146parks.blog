54b49b8325af50c41c044ca7153531c4ef17325d
Eric Scouten <eric@scouten.com>
2022-06-29 21:33:30 -0500

    Joseph Whidbey SP
    Closes #42.

---
+++ content/joseph-whidbey/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4364-159", 48.309689, -122.706872),
+        addGpxMarker(map, "es-4364-161", 48.309781, -122.706719),
+        addGpxMarker(map, "es-4364-162", 48.310089, -122.706056),
+        addGpxMarker(map, "es-4364-165", 48.311247, -122.705658),
+        addGpxMarker(map, "es-4364-168", 48.312761, -122.706108),
+        addGpxMarker(map, "es-4364-170", 48.309136, -122.713286),
+        addGpxMarker(map, "es-4364-174", 48.309094, -122.714422),
+        addGpxMarker(map, "es-4364-175p", 48.309147, -122.714561),
+        addGpxMarker(map, "es-4364-200", 48.308714, -122.713836),
+    ]
+}
