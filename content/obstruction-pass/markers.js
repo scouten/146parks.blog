174f21f0d416e0e77dbc0ae9fc89ba11519da3c3
Eric Scouten <git@scouten.me>
2022-12-17 21:28:59 -0800

    Fix a few missing items from Obstruction Pass

--- content/obstruction-pass/markers.js
+++ content/obstruction-pass/markers.js
@@ -2,12 +2,15 @@ function addGpxMarkers(map) {
     return [
         addGpxMarker(map, "es-4433-093", 48.603794, -122.829904),
         addGpxMarker(map, "es-4433-095", 48.603628, -122.829878),
+        addGpxMarker(map, "es-4433-096", 48.603573, -122.829838),
         addGpxMarker(map, "es-4433-098", 48.603345, -122.829485),
         addGpxMarker(map, "es-4433-102", 48.602170, -122.828390),
         addGpxMarker(map, "es-4433-103", 48.601942, -122.828427),
         addGpxMarker(map, "es-4433-106", 48.601741, -122.828787),
         addGpxMarker(map, "es-4433-109", 48.601745, -122.829117),
+        addGpxMarker(map, "es-4433-112", 48.601768, -122.828960),
         addGpxMarker(map, "es-4433-118", 48.603061, -122.828893),
         addGpxMarker(map, "es-4433-122", 48.603430, -122.829712),
+        addGpxMarker(map, "es-4433-130", 48.604733, -122.830216),
     ]
 }
