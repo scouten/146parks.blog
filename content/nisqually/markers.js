b469798c4297dea721da8b959da1e27e5cbc38f7
Eric Scouten <git@scouten.me>
2022-12-31 17:20:43 -0800

    Nisqually SP

---
+++ content/nisqually/markers.js
@@ -0,0 +1,18 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4461-003", 46.864681, -122.327514),
+        addGpxMarker(map, "es-4461-016", 46.860939, -122.328469),
+        addGpxMarker(map, "es-4461-022", 46.857489, -122.327011),
+        addGpxMarker(map, "es-4461-025", 46.857494, -122.327881),
+        addGpxMarker(map, "es-4461-028", 46.856969, -122.328522),
+        addGpxMarker(map, "es-4461-032", 46.855636, -122.330589),
+        addGpxMarker(map, "es-4461-037", 46.853619, -122.331169),
+        addGpxMarker(map, "es-4461-049", 46.849500, -122.332600),
+        addGpxMarker(map, "es-4461-052", 46.849539, -122.332633),
+        addGpxMarker(map, "es-4461-055", 46.853272, -122.331728),
+        addGpxMarker(map, "es-4461-057", 46.852589, -122.332961),
+        addGpxMarker(map, "es-4461-060", 46.852508, -122.333328),
+        addGpxMarker(map, "es-4461-062", 46.853189, -122.333144),
+        addGpxMarker(map, "es-4461-076", 46.861958, -122.330078),
+    ]
+}
