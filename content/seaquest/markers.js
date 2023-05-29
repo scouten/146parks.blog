01cf23ca1581f3bf6d301e489528ac2e370914a5
Eric Scouten <git@scouten.me>
2023-05-28 20:23:46 -0700

    Seaquest SP

---
+++ content/seaquest/markers.js
@@ -0,0 +1,17 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4533-023", 46.298167, -122.820000),
+        addGpxMarker(map, "es-4533-035", 46.299333, -122.819167),
+        addGpxMarker(map, "es-4533-040", 46.300000, -122.818167),
+        addGpxMarker(map, "es-4533-042", 46.300667, -122.818333),
+        addGpxMarker(map, "es-4533-046", 46.300667, -122.819000),
+        addGpxMarker(map, "es-4533-047", 46.301667, -122.819167),
+        addGpxMarker(map, "es-4533-055", 46.303000, -122.820500),
+        addGpxMarker(map, "es-4533-058", 46.303167, -122.821500),
+        addGpxMarker(map, "es-4533-064", 46.306000, -122.821167),
+        addGpxMarker(map, "es-4533-083", 46.300667, -122.814667),
+        addGpxMarker(map, "es-4533-090", 46.298833, -122.815667),
+        addGpxMarker(map, "es-4533-094", 46.298167, -122.817667),
+        addGpxMarker(map, "es-4533-098", 46.298000, -122.818667),
+    ]
+}
