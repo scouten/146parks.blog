16104314d2d3d3be2ee1d1352bed13a5b4009913
Eric Scouten <git@scouten.me>
2023-06-21 13:42:28 -0700

    Lewis And Clark Trail SP
    Closes #122

---
+++ content/lewis-and-clark-trail/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4539-020", 46.286667, -118.072167),
+        addGpxMarker(map, "es-4539-023", 46.286333, -118.074333),
+        addGpxMarker(map, "es-4539-027", 46.286667, -118.074500),
+        addGpxMarker(map, "es-4539-034", 46.288667, -118.071500),
+        addGpxMarker(map, "es-4539-038", 46.289333, -118.071667),
+        addGpxMarker(map, "es-4539-039", 46.289833, -118.072000),
+        addGpxMarker(map, "es-4539-043", 46.290167, -118.070833),
+        addGpxMarker(map, "es-4539-051", 46.289000, -118.069667),
+        addGpxMarker(map, "es-4539-056", 46.288833, -118.069833),
+    ]
+}
