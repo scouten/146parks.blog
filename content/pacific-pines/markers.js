e5937629cd4caed7384d02e3b51db75bd04c67bf
Eric Scouten <git@scouten.me>
2023-08-09 21:34:49 -0700

    Pacific Pines SP
    Closes #62

---
+++ content/pacific-pines/markers.js
@@ -0,0 +1,13 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4569-010", 46.501667, -124.056500),
+        addGpxMarker(map, "es-4569-012", 46.501500, -124.057333),
+        addGpxMarker(map, "es-4569-013", 46.501500, -124.057833),
+        addGpxMarker(map, "es-4569-017", 46.501500, -124.058333),
+        addGpxMarker(map, "es-4569-019", 46.501500, -124.058667),
+        addGpxMarker(map, "es-4569-022", 46.501333, -124.059667),
+        addGpxMarker(map, "es-4569-030", 46.501333, -124.059500),
+        addGpxMarker(map, "es-4569-032", 46.501333, -124.059000),
+        addGpxMarker(map, "es-4569-034", 46.501500, -124.056833),
+    ]
+}
