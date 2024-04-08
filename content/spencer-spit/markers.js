bea59163ec4a1200efac3b04d43edc4356a9382f
Eric Scouten <git@scouten.me>
2024-04-07 20:21:40 -0700

    Tweak photos in Spencer Spit

--- content/spencer-spit/markers.js
+++ content/spencer-spit/markers.js
@@ -1,11 +1,12 @@
 function addGpxMarkers(map) {
     return [
+        addGpxMarker(map, "es-4126-045", 48.536367, -122.859300),
+        addGpxMarker(map, "es-4127-035", 48.536000, -122.854408),
         addGpxMarker(map, "es-4126-050", 48.535361, -122.857658),
         addGpxMarker(map, "es-4126-051", 48.535306, -122.857078),
         addGpxMarker(map, "es-4127-013", 48.535352, -122.857027),
         addGpxMarker(map, "es-4127-017", 48.535497, -122.856477),
         addGpxMarker(map, "es-4126-052", 48.535714, -122.855644),
-        addGpxMarker(map, "es-4126-056", 48.535656, -122.855447),
         addGpxMarker(map, "es-4127-021", 48.535713, -122.855641),
         addGpxMarker(map, "es-4127-025", 48.536609, -122.852502),
         addGpxMarker(map, "es-4127-026", 48.536684, -122.852275),
