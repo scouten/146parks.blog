1f98e4807ed8324df006af5b05f6ebcef70293ab
Eric Scouten <git@scouten.me>
2024-12-29 16:00:33 -0800

    CDN retrofit for Spokane Plains Battlefield

--- content/spokane-plains-battlefield/markers.js
+++ content/spokane-plains-battlefield/markers.js
@@ -1,6 +1,6 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4645-009", 47.643167, -117.648000),
-        addGpxMarker(map, "es-4645-011", 47.643167, -117.648000),
+        addCdnGpxMarker(map, "es-4645-009", 47.643167, -117.648000, "img/v1/2024/05/es-4645-009"),
+        addCdnGpxMarker(map, "es-4645-011", 47.643167, -117.648000, "img/v1/2024/05/es-4645-011"),
     ]
 }
