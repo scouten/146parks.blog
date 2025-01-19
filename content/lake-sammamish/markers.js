792db7884ed37e59ae722c0344e499054b07a74b
Eric Scouten <git@scouten.me>
2025-01-19 09:00:10 -0800

    CDN retrofit for Lake Sammamish

--- content/lake-sammamish/markers.js
+++ content/lake-sammamish/markers.js
@@ -1,16 +1,16 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4352-106", 47.558300, -122.064981),
-        addGpxMarker(map, "es-4352-107", 47.560269, -122.064858),
-        addGpxMarker(map, "es-4352-111", 47.560803, -122.064553),
-        addGpxMarker(map, "es-4352-113", 47.561711, -122.065003),
-        addGpxMarker(map, "es-4352-115p", 47.561844, -122.065444),
-        addGpxMarker(map, "es-4352-147", 47.561853, -122.065453),
+        addCdnGpxMarker(map, "es-4352-106", 47.558300, -122.064981, "img/v1/2022/05/es-4352-106"),
+        addCdnGpxMarker(map, "es-4352-107", 47.560269, -122.064858, "img/v1/2022/05/es-4352-107"),
+        addCdnGpxMarker(map, "es-4352-111", 47.560803, -122.064553, "img/v1/2022/05/es-4352-111"),
+        addCdnGpxMarker(map, "es-4352-113", 47.561711, -122.065003, "img/v1/2022/05/es-4352-113"),
+        addCdnGpxMarker(map, "es-4352-115p", 47.561844, -122.065444, "img/v1/2022/05/es-4352-115p"),
+        addCdnGpxMarker(map, "es-4352-147", 47.561853, -122.065453, "img/v1/2022/05/es-4352-147"),
         addGpxMarker(map, "es-4352-148", 47.561900, -122.065500),
-        addGpxMarker(map, "es-4352-157", 47.561522, -122.064378),
-        addGpxMarker(map, "es-4352-158", 47.559294, -122.062906),
-        addGpxMarker(map, "es-4352-160", 47.559411, -122.062614),
-        addGpxMarker(map, "es-4352-162", 47.559339, -122.061378),
-        addGpxMarker(map, "es-4352-166", 47.559339, -122.058228),
+        addCdnGpxMarker(map, "es-4352-157", 47.561522, -122.064378, "img/v1/2022/05/es-4352-157"),
+        addCdnGpxMarker(map, "es-4352-158", 47.559294, -122.062906, "img/v1/2022/05/es-4352-158"),
+        addCdnGpxMarker(map, "es-4352-160", 47.559411, -122.062614, "img/v1/2022/05/es-4352-160"),
+        addCdnGpxMarker(map, "es-4352-162", 47.559339, -122.061378, "img/v1/2022/05/es-4352-162"),
+        addCdnGpxMarker(map, "es-4352-166", 47.559339, -122.058228, "img/v1/2022/05/es-4352-166"),
     ]
 }
