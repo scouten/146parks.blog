015029653794368876172faf2c025538f4f4643d
Eric Scouten <git@scouten.me>
2025-01-19 08:12:14 -0800

    CDN retrofit for Federation Forest

--- content/federation-forest/markers.js
+++ content/federation-forest/markers.js
@@ -1,13 +1,13 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4352-035", 47.151828, -121.688308),
-        addGpxMarker(map, "es-4352-044", 47.151597, -121.687683),
-        addGpxMarker(map, "es-4352-049", 47.150089, -121.689697),
-        addGpxMarker(map, "es-4352-052", 47.150367, -121.690469),
-        addGpxMarker(map, "es-4352-053", 47.150528, -121.690750),
-        addGpxMarker(map, "es-4352-059", 47.150628, -121.691047),
-        addGpxMarker(map, "es-4352-067", 47.150622, -121.687461),
-        addGpxMarker(map, "es-4352-074", 47.150744, -121.686097),
-        addGpxMarker(map, "es-4352-085", 47.151428, -121.683511),
+        addCdnGpxMarker(map, "es-4352-035", 47.151828, -121.688308, "img/v1/2022/05/es-4352-035"),
+        addCdnGpxMarker(map, "es-4352-044", 47.151597, -121.687683, "img/v1/2022/05/es-4352-044"),
+        addCdnGpxMarker(map, "es-4352-049", 47.150089, -121.689697, "img/v1/2022/05/es-4352-049"),
+        addCdnGpxMarker(map, "es-4352-052", 47.150367, -121.690469, "img/v1/2022/05/es-4352-052"),
+        addCdnGpxMarker(map, "es-4352-053", 47.150528, -121.690750, "img/v1/2022/05/es-4352-053"),
+        addCdnGpxMarker(map, "es-4352-059", 47.150628, -121.691047, "img/v1/2022/05/es-4352-059"),
+        addCdnGpxMarker(map, "es-4352-067", 47.150622, -121.687461, "img/v1/2022/05/es-4352-067"),
+        addCdnGpxMarker(map, "es-4352-074", 47.150744, -121.686097, "img/v1/2022/05/es-4352-074"),
+        addCdnGpxMarker(map, "es-4352-085", 47.151428, -121.683511, "img/v1/2022/05/es-4352-085"),
     ]
 }
