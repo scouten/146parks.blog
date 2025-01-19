668be71cf4a1716cae6d172614d66bf52a5ad6c8
Eric Scouten <git@scouten.me>
2025-01-18 21:39:04 -0800

    CDN retrofit for Joseph Whidbey

--- content/joseph-whidbey/markers.js
+++ content/joseph-whidbey/markers.js
@@ -1,13 +1,13 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4364-159", 48.309689, -122.706872),
-        addGpxMarker(map, "es-4364-161", 48.309781, -122.706719),
-        addGpxMarker(map, "es-4364-162", 48.310089, -122.706056),
-        addGpxMarker(map, "es-4364-165", 48.311247, -122.705658),
-        addGpxMarker(map, "es-4364-168", 48.312761, -122.706108),
-        addGpxMarker(map, "es-4364-170", 48.309136, -122.713286),
-        addGpxMarker(map, "es-4364-174", 48.309094, -122.714422),
-        addGpxMarker(map, "es-4364-175p", 48.309147, -122.714561),
-        addGpxMarker(map, "es-4364-200", 48.308714, -122.713836),
+        addCdnGpxMarker(map, "es-4364-159", 48.309689, -122.706872, "img/v1/2022/06/es-4364-159"),
+        addCdnGpxMarker(map, "es-4364-161", 48.309781, -122.706719, "img/v1/2022/06/es-4364-161"),
+        addCdnGpxMarker(map, "es-4364-162", 48.310089, -122.706056, "img/v1/2022/06/es-4364-162"),
+        addCdnGpxMarker(map, "es-4364-165", 48.311247, -122.705658, "img/v1/2022/06/es-4364-165"),
+        addCdnGpxMarker(map, "es-4364-168", 48.312761, -122.706108, "img/v1/2022/06/es-4364-168"),
+        addCdnGpxMarker(map, "es-4364-170", 48.309136, -122.713286, "img/v1/2022/06/es-4364-170"),
+        addCdnGpxMarker(map, "es-4364-174", 48.309094, -122.714422, "img/v1/2022/06/es-4364-174"),
+        addCdnGpxMarker(map, "es-4364-175p", 48.309147, -122.714561, "img/v1/2022/06/es-4364-175p"),
+        addCdnGpxMarker(map, "es-4364-200", 48.308714, -122.713836, "img/v1/2022/06/es-4364-200"),
     ]
 }
