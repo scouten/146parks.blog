fafa9787e20c5b0630d03b31c16720544ea868f5
Eric Scouten <git@scouten.me>
2025-01-27 14:33:18 -0800

    CDN retrofit for Belfair

--- content/belfair/markers.js
+++ content/belfair/markers.js
@@ -1,11 +1,11 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4260-199", 47.430742, -122.874328),
-        addGpxMarker(map, "es-4260-212", 47.429681, -122.874519),
-        addGpxMarker(map, "es-4260-221", 47.428792, -122.875253),
-        addGpxMarker(map, "es-4260-228", 47.429419, -122.875289),
-        addGpxMarker(map, "es-4260-234", 47.430069, -122.879081),
-        addGpxMarker(map, "es-4260-238", 47.430039, -122.881019),
-        addGpxMarker(map, "es-4260-240", 47.429764, -122.881425),
+        addCdnGpxMarker(map, "es-4260-199", 47.430742, -122.874328, "img/v1/2021/10/es-4260-199"),
+        addCdnGpxMarker(map, "es-4260-212", 47.429681, -122.874519, "img/v1/2021/10/es-4260-212"),
+        addCdnGpxMarker(map, "es-4260-221", 47.428792, -122.875253, "img/v1/2021/10/es-4260-221"),
+        addCdnGpxMarker(map, "es-4260-228", 47.429419, -122.875289, "img/v1/2021/10/es-4260-228"),
+        addCdnGpxMarker(map, "es-4260-234", 47.430069, -122.879081, "img/v1/2021/10/es-4260-234"),
+        addCdnGpxMarker(map, "es-4260-238", 47.430039, -122.881019, "img/v1/2021/10/es-4260-238"),
+        addCdnGpxMarker(map, "es-4260-240", 47.429764, -122.881425, "img/v1/2021/10/es-4260-240"),
     ]
 }
