0ed7e817d7bb36f140f7d5b43da7cf204a70ea7f
Eric Scouten <git@scouten.me>
2025-01-26 20:36:51 -0800

    CDN retrofit for Bay View

--- content/bay-view/markers.js
+++ content/bay-view/markers.js
@@ -1,13 +1,13 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4222-002", 48.487244, -122.481239),
-        addGpxMarker(map, "es-4222-009", 48.488767, -122.481872),
-        addGpxMarker(map, "es-4222-010", 48.488883, -122.481858),
-        addGpxMarker(map, "es-4222-011", 48.489331, -122.481742),
-        addGpxMarker(map, "es-4222-013", 48.489506, -122.481636),
-        addGpxMarker(map, "es-4222-015", 48.490242, -122.481925),
-        addGpxMarker(map, "es-4222-017", 48.490325, -122.481994),
-        addGpxMarker(map, "es-4222-044", 48.490008, -122.481772),
-        addGpxMarker(map, "es-4222-066", 48.488797, -122.481742),
+        addCdnGpxMarker(map, "es-4222-002", 48.487244, -122.481239, "img/v1/2021/06/es-4222-002"),
+        addCdnGpxMarker(map, "es-4222-009", 48.488767, -122.481872, "img/v1/2021/06/es-4222-009"),
+        addCdnGpxMarker(map, "es-4222-010", 48.488883, -122.481858, "img/v1/2021/06/es-4222-010"),
+        addCdnGpxMarker(map, "es-4222-011", 48.489331, -122.481742, "img/v1/2021/06/es-4222-011"),
+        addCdnGpxMarker(map, "es-4222-013", 48.489506, -122.481636, "img/v1/2021/06/es-4222-013"),
+        addCdnGpxMarker(map, "es-4222-015", 48.490242, -122.481925, "img/v1/2021/06/es-4222-015"),
+        addCdnGpxMarker(map, "es-4222-017", 48.490325, -122.481994, "img/v1/2021/06/es-4222-017"),
+        addCdnGpxMarker(map, "es-4222-044", 48.490008, -122.481772, "img/v1/2021/06/es-4222-044"),
+        addCdnGpxMarker(map, "es-4222-066", 48.488797, -122.481742, "img/v1/2021/06/es-4222-066"),
     ]
 }
