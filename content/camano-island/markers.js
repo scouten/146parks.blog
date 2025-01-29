e0759c0bec8acb5f67c1b86a5bb830092a209470
Eric Scouten <git@scouten.me>
2025-01-29 13:19:48 -0800

    CDN retrofit for Camano Island

--- content/camano-island/markers.js
+++ content/camano-island/markers.js
@@ -1,15 +1,15 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4249-136", 48.124061, -122.494889),
-        addGpxMarker(map, "es-4249-137", 48.123967, -122.494933),
-        addGpxMarker(map, "es-4249-140", 48.123644, -122.494919),
-        addGpxMarker(map, "es-4249-143", 48.123292, -122.494758),
-        addGpxMarker(map, "es-4249-149", 48.122728, -122.494339),
-        addGpxMarker(map, "es-4249-162", 48.121911, -122.493081),
-        addGpxMarker(map, "es-4249-163", 48.121967, -122.493181),
-        addGpxMarker(map, "es-4249-174", 48.121028, -122.489089),
-        addGpxMarker(map, "es-4249-179", 48.121044, -122.489931),
-        addGpxMarker(map, "es-4249-181", 48.122264, -122.493789),
-        addGpxMarker(map, "es-4249-189", 48.121503, -122.487031),
+        addCdnGpxMarker(map, "es-4249-136", 48.124061, -122.494889, "img/v1/2021/08/es-4249-136"),
+        addCdnGpxMarker(map, "es-4249-137", 48.123967, -122.494933, "img/v1/2021/08/es-4249-137"),
+        addCdnGpxMarker(map, "es-4249-140", 48.123644, -122.494919, "img/v1/2021/08/es-4249-140"),
+        addCdnGpxMarker(map, "es-4249-143", 48.123292, -122.494758, "img/v1/2021/08/es-4249-143"),
+        addCdnGpxMarker(map, "es-4249-149", 48.122728, -122.494339, "img/v1/2021/08/es-4249-149"),
+        addCdnGpxMarker(map, "es-4249-162", 48.121911, -122.493081, "img/v1/2021/08/es-4249-162"),
+        addCdnGpxMarker(map, "es-4249-163", 48.121967, -122.493181, "img/v1/2021/08/es-4249-163"),
+        addCdnGpxMarker(map, "es-4249-174", 48.121028, -122.489089, "img/v1/2021/08/es-4249-174"),
+        addCdnGpxMarker(map, "es-4249-179", 48.121044, -122.489931, "img/v1/2021/08/es-4249-179"),
+        addCdnGpxMarker(map, "es-4249-181", 48.122264, -122.493789, "img/v1/2021/08/es-4249-181"),
+        addCdnGpxMarker(map, "es-4249-189", 48.121503, -122.487031, "img/v1/2021/08/es-4249-189"),
     ]
 }
