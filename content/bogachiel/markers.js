bab8e71443cd18735368ecab5c03436e2f8e4a75
Eric Scouten <git@scouten.me>
2025-01-28 20:54:52 -0800

    CDN retrofit for Bogachiel

--- content/bogachiel/markers.js
+++ content/bogachiel/markers.js
@@ -1,15 +1,15 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4167-027", 47.893386, -124.363189),
-        addGpxMarker(map, "es-4167-028", 47.893478, -124.363319),
-        addGpxMarker(map, "es-4167-030", 47.893364, -124.363381),
-        addGpxMarker(map, "es-4167-031", 47.893353, -124.363564),
-        addGpxMarker(map, "es-4167-032", 47.893211, -124.363708),
-        addGpxMarker(map, "es-4167-045", 47.894058, -124.361900),
-        addGpxMarker(map, "es-4167-049", 47.894239, -124.358658),
-        addGpxMarker(map, "es-4167-054", 47.894150, -124.358047),
-        addGpxMarker(map, "es-4167-055", 47.894225, -124.357947),
-        addGpxMarker(map, "es-4167-058", 47.894322, -124.361236),
-        addGpxMarker(map, "es-4167-060", 47.893944, -124.362672),
+        addCdnGpxMarker(map, "es-4167-027", 47.893386, -124.363189, "img/v1/2021/01/es-4167-027"),
+        addCdnGpxMarker(map, "es-4167-028", 47.893478, -124.363319, "img/v1/2021/01/es-4167-028"),
+        addCdnGpxMarker(map, "es-4167-030", 47.893364, -124.363381, "img/v1/2021/01/es-4167-030"),
+        addCdnGpxMarker(map, "es-4167-031", 47.893353, -124.363564, "img/v1/2021/01/es-4167-031"),
+        addCdnGpxMarker(map, "es-4167-032", 47.893211, -124.363708, "img/v1/2021/01/es-4167-032"),
+        addCdnGpxMarker(map, "es-4167-045", 47.894058, -124.361900, "img/v1/2021/01/es-4167-045"),
+        addCdnGpxMarker(map, "es-4167-049", 47.894239, -124.358658, "img/v1/2021/01/es-4167-049"),
+        addCdnGpxMarker(map, "es-4167-054", 47.894150, -124.358047, "img/v1/2021/01/es-4167-054"),
+        addCdnGpxMarker(map, "es-4167-055", 47.894225, -124.357947, "img/v1/2021/01/es-4167-055"),
+        addCdnGpxMarker(map, "es-4167-058", 47.894322, -124.361236, "img/v1/2021/01/es-4167-058"),
+        addCdnGpxMarker(map, "es-4167-060", 47.893944, -124.362672, "img/v1/2021/01/es-4167-060"),
     ]
 }
