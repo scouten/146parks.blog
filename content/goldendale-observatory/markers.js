bbda156a2681ca570ed7ec8f2d4625213b4524f8
Eric Scouten <git@scouten.me>
2025-01-31 09:37:22 -0800

    CDN retrofit for Goldendale Observatory

--- content/goldendale-observatory/markers.js
+++ content/goldendale-observatory/markers.js
@@ -1,15 +1,15 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4309-102", 45.839036, -120.815619),
-        addGpxMarker(map, "es-4309-108p", 45.838656, -120.814964),
-        addGpxMarker(map, "es-4309-123", 45.838856, -120.815025),
-        addGpxMarker(map, "es-4309-124", 45.838883, -120.814972),
-        addGpxMarker(map, "es-4309-134", 45.838836, -120.815017),
-        addGpxMarker(map, "es-4309-136", 45.838836, -120.814981),
-        addGpxMarker(map, "es-4309-140", 45.838842, -120.814956),
-        addGpxMarker(map, "es-4309-142", 45.838772, -120.814903),
+        addCdnGpxMarker(map, "es-4309-102", 45.839036, -120.815619, "img/v1/2022/02/es-4309-102"),
+        addCdnGpxMarker(map, "es-4309-108p", 45.838656, -120.814964, "img/v1/2022/02/es-4309-108p"),
+        addCdnGpxMarker(map, "es-4309-123", 45.838856, -120.815025, "img/v1/2022/02/es-4309-123"),
+        addCdnGpxMarker(map, "es-4309-124", 45.838883, -120.814972, "img/v1/2022/02/es-4309-124"),
+        addCdnGpxMarker(map, "es-4309-134", 45.838836, -120.815017, "img/v1/2022/02/es-4309-134"),
+        addCdnGpxMarker(map, "es-4309-136", 45.838836, -120.814981, "img/v1/2022/02/es-4309-136"),
+        addCdnGpxMarker(map, "es-4309-140", 45.838842, -120.814956, "img/v1/2022/02/es-4309-140"),
+        addCdnGpxMarker(map, "es-4309-142", 45.838772, -120.814903, "img/v1/2022/02/es-4309-142"),
         addGpxMarker(map, "es-4309-144", 45.838800, -120.815000),
-        addGpxMarker(map, "es-4309-147", 45.838819, -120.815039),
-        addGpxMarker(map, "es-4309-153", 45.835789, -120.814506),
+        addCdnGpxMarker(map, "es-4309-147", 45.838819, -120.815039, "img/v1/2022/02/es-4309-147"),
+        addCdnGpxMarker(map, "es-4309-153", 45.835789, -120.814506, "img/v1/2022/02/es-4309-153"),
     ]
 }
