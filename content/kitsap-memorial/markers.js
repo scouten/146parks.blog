a6a7ca483db965c38e3c92cbe1cca1d00e083cb9
Eric Scouten <git@scouten.me>
2025-02-01 20:21:13 -0800

    CDN retrofit for Kitsap Memorial

--- content/kitsap-memorial/markers.js
+++ content/kitsap-memorial/markers.js
@@ -1,14 +1,14 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4169-006", 47.817875, -122.651039),
-        addGpxMarker(map, "es-4169-009", 47.817603, -122.651581),
-        addGpxMarker(map, "es-4169-011", 47.818050, -122.651711),
-        addGpxMarker(map, "es-4169-013", 47.818253, -122.651969),
-        addGpxMarker(map, "es-4169-014", 47.818161, -122.652053),
-        addGpxMarker(map, "es-4169-016", 47.817731, -122.652322),
-        addGpxMarker(map, "es-4169-022", 47.817478, -122.652611),
-        addGpxMarker(map, "es-4169-025", 47.817928, -122.652406),
-        addGpxMarker(map, "es-4169-029", 47.817919, -122.652206),
-        addGpxMarker(map, "es-4169-032", 47.818492, -122.651389),
+        addCdnGpxMarker(map, "es-4169-006", 47.817875, -122.651039, "img/v1/2021/02/es-4169-006"),
+        addCdnGpxMarker(map, "es-4169-009", 47.817603, -122.651581, "img/v1/2021/02/es-4169-009"),
+        addCdnGpxMarker(map, "es-4169-011", 47.818050, -122.651711, "img/v1/2021/02/es-4169-011"),
+        addCdnGpxMarker(map, "es-4169-013", 47.818253, -122.651969, "img/v1/2021/02/es-4169-013"),
+        addCdnGpxMarker(map, "es-4169-014", 47.818161, -122.652053, "img/v1/2021/02/es-4169-014"),
+        addCdnGpxMarker(map, "es-4169-016", 47.817731, -122.652322, "img/v1/2021/02/es-4169-016"),
+        addCdnGpxMarker(map, "es-4169-022", 47.817478, -122.652611, "img/v1/2021/02/es-4169-022"),
+        addCdnGpxMarker(map, "es-4169-025", 47.817928, -122.652406, "img/v1/2021/02/es-4169-025"),
+        addCdnGpxMarker(map, "es-4169-029", 47.817919, -122.652206, "img/v1/2021/02/es-4169-029"),
+        addCdnGpxMarker(map, "es-4169-032", 47.818492, -122.651389, "img/v1/2021/02/es-4169-032"),
     ]
 }
