a3c9f16949f7d73f15f9a7e8015cd59a0792a8f4
Eric Scouten <git@scouten.me>
2025-02-03 12:06:25 -0800

    CDN retrofit for Lime Kiln Point

--- content/lime-kiln-point/markers.js
+++ content/lime-kiln-point/markers.js
@@ -1,16 +1,16 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4126-100", 48.515675, -123.152061),
-        addGpxMarker(map, "es-4127-044", 48.515707, -123.151912),
-        addGpxMarker(map, "es-4127-046", 48.515707, -123.151912),
-        addGpxMarker(map, "es-4126-105", 48.515808, -123.152222),
-        addGpxMarker(map, "es-4126-113", 48.518672, -123.151322),
-        addGpxMarker(map, "es-4126-129", 48.518725, -123.151397),
-        addGpxMarker(map, "es-4126-133", 48.518694, -123.151406),
-        addGpxMarker(map, "es-4126-141", 48.518647, -123.151528),
-        addGpxMarker(map, "es-4126-144", 48.518725, -123.151536),
+        addCdnGpxMarker(map, "es-4126-100", 48.515675, -123.152061, "img/v1/2020/10/es-4126-100"),
+        addCdnGpxMarker(map, "es-4127-044", 48.515707, -123.151912, "img/v1/2020/10/es-4127-044"),
+        addCdnGpxMarker(map, "es-4127-046", 48.515707, -123.151912, "img/v1/2020/10/es-4127-046"),
+        addCdnGpxMarker(map, "es-4126-105", 48.515808, -123.152222, "img/v1/2020/10/es-4126-105"),
+        addCdnGpxMarker(map, "es-4126-113", 48.518672, -123.151322, "img/v1/2020/10/es-4126-113"),
+        addCdnGpxMarker(map, "es-4126-129", 48.518725, -123.151397, "img/v1/2020/10/es-4126-129"),
+        addCdnGpxMarker(map, "es-4126-133", 48.518694, -123.151406, "img/v1/2020/10/es-4126-133"),
+        addCdnGpxMarker(map, "es-4126-141", 48.518647, -123.151528, "img/v1/2020/10/es-4126-141"),
+        addCdnGpxMarker(map, "es-4126-144", 48.518725, -123.151536, "img/v1/2020/10/es-4126-144"),
         addGpxMarker(map, "es-4130-009", 48.515200, -123.151700),
-        addGpxMarker(map, "es-4130-015", 48.515336, -123.151772),
-        addGpxMarker(map, "es-4128-034", 48.515347, -123.151719),
+        addCdnGpxMarker(map, "es-4130-015", 48.515336, -123.151772, "img/v1/2020/10/es-4130-015"),
+        addCdnGpxMarker(map, "es-4128-034", 48.515347, -123.151719, "img/v1/2020/10/es-4128-034"),
     ]
 }
