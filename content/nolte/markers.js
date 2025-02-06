8620486ba7f930c143df9406df8c6f77b15544c7
Eric Scouten <git@scouten.me>
2025-02-05 17:25:28 -0800

    CDN retrofit for Nolte

--- content/nolte/markers.js
+++ content/nolte/markers.js
@@ -1,11 +1,11 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4285-080", 47.270489, -121.940964),
-        addGpxMarker(map, "es-4285-083", 47.271369, -121.941567),
-        addGpxMarker(map, "es-4285-084", 47.272111, -121.942008),
-        addGpxMarker(map, "es-4285-088", 47.273525, -121.941497),
-        addGpxMarker(map, "es-4285-090", 47.277447, -121.941261),
-        addGpxMarker(map, "es-4285-091", 47.277694, -121.940314),
-        addGpxMarker(map, "es-4285-094", 47.277194, -121.938600),
+        addCdnGpxMarker(map, "es-4285-080", 47.270489, -121.940964, "img/v1/2021/12/es-4285-080"),
+        addCdnGpxMarker(map, "es-4285-083", 47.271369, -121.941567, "img/v1/2021/12/es-4285-083"),
+        addCdnGpxMarker(map, "es-4285-084", 47.272111, -121.942008, "img/v1/2021/12/es-4285-084"),
+        addCdnGpxMarker(map, "es-4285-088", 47.273525, -121.941497, "img/v1/2021/12/es-4285-088"),
+        addCdnGpxMarker(map, "es-4285-090", 47.277447, -121.941261, "img/v1/2021/12/es-4285-090"),
+        addCdnGpxMarker(map, "es-4285-091", 47.277694, -121.940314, "img/v1/2021/12/es-4285-091"),
+        addCdnGpxMarker(map, "es-4285-094", 47.277194, -121.938600, "img/v1/2021/12/es-4285-094"),
     ]
 }
