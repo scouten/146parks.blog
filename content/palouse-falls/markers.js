0841bc335345e6787b6d6f02e4b9ba682a836ed2
Eric Scouten <git@scouten.me>
2025-02-05 21:19:23 -0800

    CDN retrofit for Palouse Falls

--- content/palouse-falls/markers.js
+++ content/palouse-falls/markers.js
@@ -1,13 +1,13 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4183-024p", 46.663506, -118.226678),
+        addCdnGpxMarker(map, "es-4183-024p", 46.663506, -118.226678, "img/v1/2021/03/es-4183-024p"),
         addGpxMarker(map, "es-4183-036", 46.663500, -118.226700),
-        addGpxMarker(map, "es-4183-037", 46.663361, -118.226853),
-        addGpxMarker(map, "es-4183-050p", 46.662411, -118.228211),
-        addGpxMarker(map, "es-4184-002", 46.662414, -118.228219),
-        addGpxMarker(map, "es-4183-070", 46.664547, -118.227661),
-        addGpxMarker(map, "es-4183-072", 46.665117, -118.226564),
-        addGpxMarker(map, "es-4183-074", 46.665853, -118.225608),
-        addGpxMarker(map, "es-4183-076", 46.667275, -118.226944),
+        addCdnGpxMarker(map, "es-4183-037", 46.663361, -118.226853, "img/v1/2021/03/es-4183-037"),
+        addCdnGpxMarker(map, "es-4183-050p", 46.662411, -118.228211, "img/v1/2021/03/es-4183-050p"),
+        addCdnGpxMarker(map, "es-4184-002", 46.662414, -118.228219, "img/v1/2021/03/es-4184-002"),
+        addCdnGpxMarker(map, "es-4183-070", 46.664547, -118.227661, "img/v1/2021/03/es-4183-070"),
+        addCdnGpxMarker(map, "es-4183-072", 46.665117, -118.226564, "img/v1/2021/03/es-4183-072"),
+        addCdnGpxMarker(map, "es-4183-074", 46.665853, -118.225608, "img/v1/2021/03/es-4183-074"),
+        addCdnGpxMarker(map, "es-4183-076", 46.667275, -118.226944, "img/v1/2021/03/es-4183-076"),
     ]
 }
