7d3a68df60d0630bf1b80a347287d9cae7035427
Eric Scouten <git@scouten.me>
2025-02-07 12:24:50 -0800

    CDN retrofit for Reed Island

--- content/reed-island/markers.js
+++ content/reed-island/markers.js
@@ -1,15 +1,15 @@
 function addGpxMarkers(map) {
     return [
-        addGpxMarker(map, "es-4204-026p", 45.560286, -122.334167),
+        addCdnGpxMarker(map, "es-4204-026p", 45.560286, -122.334167, "img/v1/2021/05/es-4204-026p"),
         addGpxMarker(map, "es-4204-055", 45.557500, -122.319000),
-        addGpxMarker(map, "es-4204-056", 45.557369, -122.312456),
-        addGpxMarker(map, "es-4204-057p", 45.558033, -122.294861),
-        addGpxMarker(map, "es-4204-069", 45.554797, -122.287475),
-        addGpxMarker(map, "es-4204-072p", 45.550072, -122.291192),
-        addGpxMarker(map, "es-4204-091", 45.549581, -122.300186),
-        addGpxMarker(map, "es-4204-097", 45.549656, -122.300347),
-        addGpxMarker(map, "es-4204-105", 45.555497, -122.330794),
-        addGpxMarker(map, "es-4204-108", 45.556492, -122.331261),
-        addGpxMarker(map, "es-4204-116", 45.557322, -122.332383),
+        addCdnGpxMarker(map, "es-4204-056", 45.557369, -122.312456, "img/v1/2021/05/es-4204-056"),
+        addCdnGpxMarker(map, "es-4204-057p", 45.558033, -122.294861, "img/v1/2021/05/es-4204-057p"),
+        addCdnGpxMarker(map, "es-4204-069", 45.554797, -122.287475, "img/v1/2021/05/es-4204-069"),
+        addCdnGpxMarker(map, "es-4204-072p", 45.550072, -122.291192, "img/v1/2021/05/es-4204-072p"),
+        addCdnGpxMarker(map, "es-4204-091", 45.549581, -122.300186, "img/v1/2021/05/es-4204-091"),
+        addCdnGpxMarker(map, "es-4204-097", 45.549656, -122.300347, "img/v1/2021/05/es-4204-097"),
+        addCdnGpxMarker(map, "es-4204-105", 45.555497, -122.330794, "img/v1/2021/05/es-4204-105"),
+        addCdnGpxMarker(map, "es-4204-108", 45.556492, -122.331261, "img/v1/2021/05/es-4204-108"),
+        addCdnGpxMarker(map, "es-4204-116", 45.557322, -122.332383, "img/v1/2021/05/es-4204-116"),
     ]
 }
